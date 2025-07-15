1178ac91db9e655722c52a3e8f8737d43eadc8fa
Anshul Sharma <ans.anshul07@gmail.com>
2025-07-16 00:08:37 +0530

    Create basic struture.cpp

---
+++ basic struture.cpp
@@ -0,0 +1,7 @@
+#include <iostream>                  // for input/output
+using namespace std;                // to avoid writing std:: again and again
+
+int main() {                       // program starts from here
+    cout << "Hello, Anshul!";
+    return 0;                     // ends the program
+}
