a98c94bb0ea36befdb58ec0ec6c90cf71c5d537b
Anshul Sharma <ans.anshul07@gmail.com>
2025-07-16 00:37:13 +0530

    Create input-output.cpp

---
+++ input-output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// cout- for output
+// cin- for input
+
+int main() {
+    string name;                             // Use string instead of char
+    cout << "What is your name? ";
+    cin >> name;
+    cout << "Entered name is: " << name;
+    return 0;
+}
